e960cb760d9ff2efe8274ea8ef9074672615301d
Pierre Laas <pierrel@intenda.za.com>
2026-01-03 12:39:10 +0200

    chore: Remove old tracing code

--- photo-bench-ui/src-tauri/src/lib.rs
+++ photo-bench-ui/src-tauri/src/lib.rs
@@ -1,7 +1,4 @@
 use tauri_plugin_shell::ShellExt;
-use tracing_subscriber::{
-    fmt::format::FmtSpan, prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt,
-};
 
 use tauri::Manager;
 
@@ -18,45 +15,6 @@ struct AppState {}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    // let mut log_dir = dirs::data_local_dir().unwrap();
-    // log_dir.push("com.gmail-attachment-downloader.app");
-    // fs::create_dir_all(&log_dir).unwrap();
-    // log_dir.push("gdownloader.log");
-    // let mut append = true;
-    // let mut truncate = false;
-    // let mb_size = if log_dir.exists() {
-    //     let meta = log_dir.metadata().unwrap();
-    //     let mb = meta.size() as f64 / 1024.0 / 1024.0;
-    //     if mb > 100.0 {
-    //         append = false;
-    //         truncate = true;
-    //     }
-    //     mb
-    // } else {
-    //     0.
-    // };
-    // let debug_file = OpenOptions::new()
-    //     .append(append)
-    //     .truncate(truncate)
-    //     .create(true)
-    //     .open(&log_dir)
-    //     .unwrap();
-    // let (non_blocking, _guard) = tracing_appender::non_blocking(debug_file);
-    // tracing_subscriber::registry()
-    //     // .with(
-    //     //     fmt::layer()
-    //     //         .with_writer(non_blocking)
-    //     //         .with_ansi(false)
-    //     //         .with_filter(LevelFilter::from_level(Level::DEBUG)),
-    //     // )
-    //     .with(
-    //         tracing_subscriber::EnvFilter::try_from_default_env()
-    //             .unwrap_or_else(|_| "info,tiberius=error,odbc_api=error".into()),
-    //     )
-    //     .with(tracing_subscriber::fmt::layer().with_span_events(FmtSpan::CLOSE))
-    //     .init();
-    // info!("Logs initialized at {log_dir:?}. Log file size: {mb_size}MB");
-
     tauri::Builder::default()
         .setup(|app| {
             app.manage(AppState {});
